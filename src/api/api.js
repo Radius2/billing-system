8a0f937b622b7cec1214573bb9f10106cd0c7759
Radius2 <orenvov@gmail.com>
2021-05-26 12:12:05 +0500

    init

--- src/api/api.js
+++ src/api/api.js
@@ -1,41 +1,42 @@
 import axios from 'axios';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-//const BASE_URL = 'http://b644ad81271d.ngrok.io/login/';
-const api = axios.create({})
+const BASE_URL = 'http://b644ad81271d.ngrok.io/login/';
+const api = axios.create({
+  baseURL: BASE_URL,
+});
 
 function setToken(token) {
-    localStorage.setItem('token', token);
-    api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+  localStorage.setItem('token', token);
+  api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
 }
 
 function deleteToken() {
-    localStorage.removeItem('token');
-    delete api.defaults.headers.common['Authorization'];
+  localStorage.removeItem('token');
+  delete api.defaults.headers.common['Authorization'];
 }
 
 export function getToken(username, password) {
-    return jwt.sign({username: username, password: password}, 'jwt_secret_key')
+  return jwt.sign({ username: username, password: password }, 'jwt_secret_key');
 }
 
-
 export function login(token) {
-    return api.get('/login/', {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
+  return api
+    .get('/login/', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     })
-        .then(resp => {
-            setToken(token)
-            return resp
-        })
-        .catch(err => {
-            deleteToken();
-            return Promise.reject(err)
-        });
+    .then(resp => {
+      setToken(token);
+      return resp;
+    })
+    .catch(err => {
+      deleteToken();
+      return Promise.reject(err);
+    });
 }
 
 export function getForm(path) {
-    return api.get('form_types')
-        .then(resp => console.log(resp))
+  return api.get('form_types').then(resp => console.log(resp));
 }
